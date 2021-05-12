3b4dbe2168d78f8d7db72bf3cf106bc3ea581f93
outdangdang <ppbaozi@gmail.com>
2021-05-12 20:43:46 +0800

    upload

---
+++ ffmpeg_script.sh
@@ -0,0 +1,2 @@
+ffmpeg -i tiebiaotongmu.mp4 -c:v libx264 -c:a aac -crf 30 -b:a 32k -vf scale=-1:240 tiebiaotongmu-out.mp4 # h264 压缩
+ffmpeg -i tiebiaotongmu.mp4 -c:v libvpx-vp9 -c:a libopus -b:v 200k -b:a 32k -vf scale=-1:240 tiebiaotongmu-out.webm # vp9 压缩
