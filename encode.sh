0b32f83e495c8d694d2f21660155d627ef3a4e0d
梁胡刚 <lianghugang@dxy.cn>
2021-05-15 16:57:16 +0800

    upload

--- encode.sh
+++ encode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for file in videos/*;
+for file in videos/*/*;
 do
 if [ -f "$file/source.mp4" ] && [ ! -f "$file/playlist.m3u8" ];then
 ffmpeg -i $file/source.mp4 -c:v libx264 -c:a aac -crf 30 -b:a 64k -vf scale=-1:240 -f ssegment -segment_format mpegts -segment_list $file/playlist.m3u8 -segment_time 10 $file/%03d.ts
